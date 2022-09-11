1d24a15ea67d92c6b6214ee004f7fc5f864b4147
Ali M <ali.mo.dev@gmail.com>
2022-09-11 15:44:55 +0430

    Added Services Configuration in Web

--- src/Web/Program.cs
+++ src/Web/Program.cs
@@ -1,4 +1,15 @@
+using Application.Services;
+using Application.Settings;
+
 var builder = WebApplication.CreateBuilder(args);
+builder.Configuration.AddUserSecrets<Program>();
+
+builder.Services.Configure<WeatherSettings>(builder.Configuration.GetSection("WeatherSettings"));
+builder.Services.AddHttpClient<IWeatherClient, WeatherClient>(opt =>
+{
+    opt.BaseAddress = new Uri(builder.Configuration["WeatherSettings:BaseUrl"]);
+});
+
 var app = builder.Build();
 
 app.MapGet("/", () => "Hello World!");
